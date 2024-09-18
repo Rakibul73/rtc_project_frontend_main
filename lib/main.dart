ec9fad47ebd89d6c6400c36ccb19a16c4e7b8e9e
Rakibul73 <rakibul29185@yahoo.com>
2024-09-18 15:58:46 +0600

    revert back url path

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rtc_project_fronend/root_app.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
   runApp(const RootApp());
 }
